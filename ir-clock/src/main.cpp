c757c747035c0dab62c6bd80a917a77c232d449f
Sebastian Jana <s.jana@oth-aw.de>
2025-08-01 21:30:15 +0200

    configure time setting

--- ir-clock/src/main.cpp
+++ ir-clock/src/main.cpp
@@ -3,6 +3,10 @@
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
 
+#include <WiFi.h>
+#include <time.h>
+#include "wifi_config.h"
+
 #define IR_SEND_PIN 23  // Passenden Pin für deinen IR LED-Sender wählen
 
 IRsend irsend(IR_SEND_PIN);
@@ -33,19 +37,104 @@ uint32_t getIRCode(const char* name);
 void setup() {
   Serial.begin(115200);
   delay(200);
+
+  // WiFi Setup
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.printf("Connecting with WiFI \"%s\" …\n", WIFI_SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+  Serial.println("Connected!");
+  Serial.printf("IP-Address: %s\n", WiFi.localIP().toString().c_str());
+
+  delay(500);
+
+  // NTP konfigurieren
+  configTime(0, 0, "pool.ntp.org");  
+  setenv("TZ", "CET-1CEST,M3.5.0/2,M10.5.0/3", 1);
+  tzset();
+  Serial.println("NTP configured, waiting for time...");
+
+  // Auf gültige Zeit warten
+  struct tm timeinfo;
+  while (!getLocalTime(&timeinfo)) {
+    delay(500);
+  }
+  Serial.println("Time synchronized!");
+
+  delay(500);
+
+  // Infrared Sender Setup
   irsend.begin();
-  Serial.println("IR sender ready.");
+  Serial.println("IR-Sender ready");
 }
 
-void loop() {  
-  uint32_t code = 0xFD027F80;
-  
-  // Test 2: Bit-reversed
-  uint32_t reversed_code = reverseBits(code);
-  irsend.sendNEC(reversed_code, 32);
+void loop() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Time not available");
+    return;
+  }
 
-  delay(1000);
+  // 1) Berechne Sekunden seit Mitternacht und Noon-Offset
+  int nowSec   = timeinfo.tm_hour * 3600 
+               + timeinfo.tm_min  *   60 
+               + timeinfo.tm_sec;
+  const int noonSec = 12 * 3600;       // 12:00 Uhr
+
+  int deltaSec = nowSec - noonSec;      // >0 nach Mittag, <0 vor Mittag
+  bool afterNoon = (deltaSec >= 0);
+  int absSec = afterNoon ? deltaSec : -deltaSec;
+
+  // Stunden und Minuten Unterschied
+  int diffH = absSec / 3600;
+  int diffM = (absSec % 3600) / 60;
+
+  // Debug
+  if (afterNoon) {
+    Serial.printf("Seit 12:00: %d h, %d m\n", diffH, diffM);
+  } else {
+    Serial.printf("Bis 12:00: %d h, %d m\n", diffH, diffM);
+  }
+
+  // 2) Codes vorbereiten
+  uint32_t codeClock     = reverseBits(getIRCode("CLOCK"));
+  uint32_t codeUp        = reverseBits(getIRCode("ARROW_UP"));
+  uint32_t codeDown      = reverseBits(getIRCode("ARROW_DOWN"));
+  uint32_t codeRight     = reverseBits(getIRCode("ARROW_RIGHT"));
+
+  // 3) In Clock-Setting-Modus wechseln
+  irsend.sendNEC(codeClock, 32);
+  delay(200);
+
+  // 4) Stunden einstellen
+  for (int i = 0; i <= diffH; i++) {
+    irsend.sendNEC(afterNoon ? codeUp : codeDown, 32);
+    delay(100);
+  }
+
+  // 5) Auf Minuten-Modus umschalten
+  irsend.sendNEC(codeRight, 32);
+  delay(200);
+
+  // 6) Minuten einstellen
+  for (int i = 0; i <= diffM; i++) {
+    irsend.sendNEC(afterNoon ? codeUp : codeDown, 32);
+    delay(100);
+  }
+
+  // 7) Einstell-Modus verlassen / speichern
+  irsend.sendNEC(codeClock, 32);
+  delay(200);
+
+  irsend.sendNEC(codeClock, 32);
+  delay(200);
+
+  // 8) Sleep oder nächste Iteration
+  delay(100000000);
 }
+
 
 // Reverse the bits of the code to be sent
 uint32_t reverseBits(uint32_t value) {
