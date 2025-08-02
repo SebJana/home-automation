1f613b48d4ed2886e4f3a17325aa1fe48bddc697
Sebastian Jana <s.jana@oth-aw.de>
2025-08-02 15:50:52 +0200

    refactor clock setting logic

--- ir-clock/src/main.cpp
+++ ir-clock/src/main.cpp
@@ -17,6 +17,11 @@ struct IRCode {
   uint32_t code;
 };
 
+struct ClockDifference { 
+  int hourDifference;
+  int minuteDifference;
+};
+
 // Every code the remote sends
 const IRCode ir_codes[] = {
   {"ARROW_UP", 0xFD027F80},
@@ -31,10 +36,22 @@ const IRCode ir_codes[] = {
 
 const int IR_CODES_COUNT = sizeof(ir_codes) / sizeof(ir_codes[0]);
 const int NOON_SEC = 12 * 3600; // Seconds since Midnight that equal '12:00' 
-const int WAITING_INTERVAL = 1000; // Milliseconds to wait in-between sending IR-codes
+const int WAITING_INTERVAL = 25; // Milliseconds to wait in-between sending IR-codes
+
+// Global final codes to reduce lookups
+uint32_t codeClock;
+uint32_t codeUp;
+uint32_t codeDown;
+uint32_t codeRight;
 
 uint32_t getIRCode(const char* name);
 uint32_t reverseBits(uint32_t value);
+void sendCode(uint32_t code);
+ClockDifference determineTimeOffset(struct tm time);
+int determineMinuteSteps(int diffM);
+void setClockDigits(int amount, bool directionUp);
+
+void setClockToCurrentTime(struct tm time);
 
 void setup() {
   Serial.begin(115200);
@@ -52,13 +69,13 @@ void setup() {
 
   delay(500);
 
-  // NTP konfigurieren
+  // Configure NTP
   configTime(0, 0, "pool.ntp.org");  
   setenv("TZ", "CET-1CEST,M3.5.0/2,M10.5.0/3", 1);
   tzset();
   Serial.println("NTP configured, waiting for time...");
 
-  // Auf gültige Zeit warten
+  // Wait till a valid time is received
   struct tm timeinfo;
   while (!getLocalTime(&timeinfo)) {
     delay(500);
@@ -70,6 +87,12 @@ void setup() {
   // Infrared Sender Setup
   irsend.begin();
   Serial.println("IR-Sender ready");
+
+  // Prepare Infrared Codes
+  codeClock = reverseBits(getIRCode("CLOCK"));
+  codeUp    = reverseBits(getIRCode("ARROW_UP"));
+  codeDown  = reverseBits(getIRCode("ARROW_DOWN"));
+  codeRight = reverseBits(getIRCode("ARROW_RIGHT"));
 }
 
 void loop() {
@@ -79,89 +102,15 @@ void loop() {
     return;
   }
 
-  // Calc the offset from '12:00' (Noon)
-  int nowSec = getSecondsSinceMidnight(time); // Seconds since Midnight
-  int deltaSec = nowSec - NOON_SEC; // >0 after noon, <0 before noon
-
-  int diffH = calculateHourDifference(deltaSec);
-  int diffM = calculateMinuteDifference(deltaSec);
-
-  // Need to set clock to one "less" hour if its before noon
-  if (diffH < 0 && diffM != 0){
-    diffH = diffH - 1;
-  }
-
-  // Debug
-  Serial.printf("%d h, %d m\n", diffH, diffM);
-
-  // Prepare codes
-  uint32_t codeClock     = reverseBits(getIRCode("CLOCK"));
-  uint32_t codeUp        = reverseBits(getIRCode("ARROW_UP"));
-  uint32_t codeDown      = reverseBits(getIRCode("ARROW_DOWN"));
-  uint32_t codeRight     = reverseBits(getIRCode("ARROW_RIGHT"));
-
-  // Switch from AM/PM to 24h Clock
-  irsend.sendNEC(codeUp, 32);
-  delay(WAITING_INTERVAL);
-
-  // Enable clock setting mode
-  irsend.sendNEC(codeClock, 32);
-  delay(WAITING_INTERVAL);
-
-  // Set hours
-  for (int i = 0; i < std::abs(diffH); i++) {
-    if(diffH < 0){
-      irsend.sendNEC(codeDown, 32);
-    }
-    if(diffH > 0){
-      irsend.sendNEC(codeUp, 32);
-    }
-    delay(WAITING_INTERVAL);
-  }
-
-  delay(WAITING_INTERVAL);
-  delay(WAITING_INTERVAL);
-  // Switch to minute-setting mode
-  irsend.sendNEC(codeRight, 32);
-  delay(WAITING_INTERVAL);
-
-  // Setting Minutes -40 mins is the same as setting +20 mins
-  // Determine which one requires less steps
-  int minuteSteps = diffM;
-  int changedDirectionSteps = 0;
-  if(diffM < 0){
-    changedDirectionSteps = 60 + diffM; // e.g 60 + (-40) = +20
-  }
-  if(diffM > 0){
-    changedDirectionSteps = -(60 - diffM); // e.g -(60 - (+40)) = -20
-  }
-
-  // Choose changed direction if it requires less steps
-  if(std::abs(changedDirectionSteps) < std::abs(diffM)){
-    minuteSteps = changedDirectionSteps;
-  }
-
-  // Set minutes
-  for (int i = 0; i < std::abs(minuteSteps); i++) {
-    if(minuteSteps < 0){
-      irsend.sendNEC(codeDown, 32);
-    }
-    if(minuteSteps > 0){
-      irsend.sendNEC(codeUp, 32);
-    }
-    delay(WAITING_INTERVAL);
-  }
+  setClockToCurrentTime(time);
 
   // Leave clock settings (skip date setting for now)
-  irsend.sendNEC(codeClock, 32);
-  delay(WAITING_INTERVAL);
-  irsend.sendNEC(codeClock, 32);
-  delay(WAITING_INTERVAL);
-  irsend.sendNEC(codeClock, 32);
+  for(int i = 0; i < 3; i++){
+    sendCode(codeClock);
+  }
 
   delay(100000000);
 }
-
 
 // Reverse the bits of the code to be sent
 uint32_t reverseBits(uint32_t value) {
@@ -180,4 +129,85 @@ uint32_t getIRCode(const char* name) {
     }
   }
   return 0; // No code found by that name
+}
+
+void sendCode(uint32_t code){
+  irsend.sendNEC(code, 32);
+  delay(WAITING_INTERVAL);
+}
+
+ClockDifference determineTimeOffset(struct tm time){
+  int nowSec = getSecondsSinceMidnight(time); // Seconds since Midnight
+  int deltaSec = nowSec - NOON_SEC; // >0 after noon, <0 before noon
+
+  int diffH = calculateHourDifference(deltaSec);
+  int diffM = calculateMinuteDifference(deltaSec);
+
+  // Need to set clock to one "less" hour if its before noon 
+  // e.g it's 10:40 then the full hour diff to 12:00 is only one --> hour clock needs to be changed by two hours
+  if (diffH < 0 && diffM != 0){
+    diffH = diffH - 1;
+  }
+
+  return {diffH, diffM};
+}
+
+void setClockDigits(int amount, bool directionUp){
+  // Determine clock setting direction code
+  uint32_t code = 0;
+  if(directionUp){
+    code = codeUp;
+  }
+  else{
+    code = codeDown;
+  }
+
+  // Set the clock by the amount specified
+  for (int i = 0; i < std::abs(amount); i++) {
+    sendCode(code);
+  }
+}
+
+int determineMinuteSteps(int diffM){
+  // Setting Minutes -40 mins is the same as setting +20 mins
+  // Determine which one requires less steps
+  int changedDirectionSteps = 0;
+  if(diffM < 0){
+    changedDirectionSteps = 60 + diffM; // e.g 60 + (-40) = +20
+  }
+  if(diffM > 0){
+    changedDirectionSteps = -(60 - diffM); // e.g -(60 - (+40)) = -20
+  }
+
+  // Choose changed direction if it requires less steps
+  if(std::abs(changedDirectionSteps) < std::abs(diffM)){
+    return changedDirectionSteps;
+  }
+  return diffM; // Default if the changed direction isn't faster
+}
+
+void setClockToCurrentTime(struct tm time){
+  ClockDifference cd = determineTimeOffset(time);
+  int diffH = cd.hourDifference;
+  int diffM = cd.minuteDifference;
+
+  // Debug
+  Serial.printf("%d h, %d m\n", diffH, diffM);
+
+  // Switch from AM/PM to 24h Clock
+  sendCode(codeUp);
+  // Enable clock setting mode
+  sendCode(codeClock);
+
+  // Set hours
+  bool hourDirection = (diffH > 0);
+  setClockDigits(diffH, hourDirection);
+
+  // Switch to minute-setting mode
+  sendCode(codeRight);
+
+  // Set minutes
+  int minuteDigitSteps = determineMinuteSteps(diffM);
+  bool minuteDirection = (minuteDigitSteps > 0);
+  setClockDigits(minuteDigitSteps, minuteDirection);
 }
