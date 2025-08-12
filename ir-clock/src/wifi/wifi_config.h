be5135d061c4c0e946a51bf5ca430266cbad3121
SebJana <157301190+SebJana@users.noreply.github.com>
2025-08-12 20:59:00 +0200

    fix

--- ir-clock/src/wifi/wifi_config.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-// WIFI Access Credentials
-static const char* WIFI_SSID     = "FRITZ!Box 7530 YU";
-static const char* WIFI_PASSWORD = "18835552399679259873";
-
-#endif
